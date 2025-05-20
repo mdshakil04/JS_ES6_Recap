627dd4185c8fa59ce9e337609aa1d3dd5d55038b
mdshakil04 <m.shakil04@yahoo.com>
2025-05-20 23:47:05 +0600

    Spreed operator done

--- JS/arrow.js
+++ JS/arrow.js
@@ -2,4 +2,11 @@
 // -----------------Arrow Function-----------------------------
 const add = (a, b) => a + b;
 const sum = add(5, 10);
-console.log(sum)
+// console.log(sum)
+const numbers1 = [1, 3, 5, 7, 9];
+const numbers2 = [2, 4, 6, 8, 10]
+const allNumbers = [...numbers1 , ...numbers2];
+// console.log(allNumbers)
+const orginalString = "Shakil";
+const strSpreed = [...orginalString];
+console.log(strSpreed)
