24329f0f93157c988c66b9453c48916fd923c5bc
mdshakil04 <m.shakil04@yahoo.com>
2025-05-22 16:45:05 +0600

    map added

---
+++ JS/chain.js
@@ -0,0 +1,6 @@
+const data = [{id: 1, name: 'Abdul', address: 'Bhola'}, {id:2, name:"Shakil", address:"Dhaka"}]
+// console.log(data[0].name)
+// console.log(data[1].name)
+const numbers = [12, 34, 45, 24, 21, 40]
+const doubledIt = numbers.map(num => num * 2) 
+console.log(doubledIt)
