454411c06726eee3cf8d467eddb113781c4184a0
mdshakil04 <m.shakil04@yahoo.com>
2025-05-23 09:59:37 +0600

    forEach added

--- JS/chain.js
+++ JS/chain.js
@@ -1,12 +1,3 @@
 const data = [{id: 1, name: 'Abdul', address: 'Bhola'}, {id:2, name:"Shakil", address:"Dhaka"}]
 // console.log(data[0].name)
 // console.log(data[1].name)
-const numbers = [12, 34, 45, 24, 21, 40]
-const doubledIt = numbers.map(num => num * 2) 
-// console.log(doubledIt)
-// ---------------other option-------------------------
-function doubleIt2(num){
-    return num * 2;
-}
-const result = numbers.map(doubleIt2)
-console.log(result)
---
+++ JS/forEach.js
@@ -0,0 +1,5 @@
+const friends = ['Shakil', 'Mizan', 'Sobuj', 'Sobuj']
+const friend = friends.forEach(f => {
+    console.log(f)
+})
+console.log(friend)
