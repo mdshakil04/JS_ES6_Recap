f397f27c3d0b3d8e0fcc31efc0bfbbf19f19a633
mdshakil04 <m.shakil04@yahoo.com>
2025-05-23 10:06:08 +0600

    forEach done

--- JS/forEach.js
+++ JS/forEach.js
@@ -1,5 +1,7 @@
-const friends = ['Shakil', 'Mizan', 'Sobuj', 'Sobuj']
-const friend = friends.forEach(f => {
-    console.log(f)
-})
-console.log(friend)
+const friends = ['Shakil', 'Mizan', 'Sobuj', 'Shofiq',"Zahid"]
+// const friend = friends.forEach(f => {
+//     // console.log(f)
+// })
+// console.log(friend)
+const oddFriends = friends.filter(f => f.length % 2 === 1)
+console.log(oddFriends)
