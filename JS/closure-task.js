33d60f7ed6cf569d0436cf51eecf3d949dfab9f3
mdshakil04 <m.shakil04@yahoo.com>
2025-06-01 17:02:57 +0600

    Problem two solved

--- JS/closure-task.js
+++ JS/closure-task.js
@@ -1,3 +1,4 @@
+// -------------------Problem 1--------------------------
 function createGreeter(name){
     let personName = name;
     function innerFunction(){
@@ -7,3 +8,25 @@ function createGreeter(name){
 }
 const greetShakil = createGreeter("Shakil")
 greetShakil()
+// -----------------Problem 2-----------------------------
+function createLimitedCounter(limit){
+    let count = 0;
+    function counter(){
+        if(count < limit){
+            count ++
+            console.log(count)
+        }else{
+            console.log("Limit reached")
+        }
+        // count++
+        // console.log(count)
+    }
+    return counter
+}
+
+const counter = createLimitedCounter(3)
+counter()
+counter()
+counter()
+counter()
+counter()
